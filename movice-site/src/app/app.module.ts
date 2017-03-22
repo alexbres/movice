7d10eaaca705c459bbb7154bc0057536e0ba062b
Alexey Breslavsky <belairinternet\alexeyb>
2017-03-22 12:00:20 -0700

    added films component

--- movice-site/src/app/app.module.ts
+++ movice-site/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FilmService }  from './film.service';
 import { AppComponent }  from './app.component';
+import { FilmsComponent } from './films.component';
 import { FilmDetailComponent } from './film-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+    BrowserModule 
+  ],
   declarations: [ 
     AppComponent,
+    FilmsComponent, 
     FilmDetailComponent 
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [
+    FilmService
+  ],
 })
 export class AppModule { }
