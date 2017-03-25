156f55ba232a04bd1d711474bee8a3a78492139a
Alexey Breslavsky <belairinternet\alexeyb>
2017-03-25 09:01:57 -0700

    impl Dashboard Top Heroes, fix issue with proper path to html files

--- movice-site/src/app/dashboard.component.ts
+++ movice-site/src/app/dashboard.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Film } from './film';
+import { FilmService } from './film.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  template: './dashboard.component.html',
 })
-export class DashboardComponent { }
+
+export class DashboardComponent implements OnInit {
+
+  films: Film[] = [];
+
+  constructor(private filmService: FilmService) { }
+
+  ngOnInit(): void {
+    this.filmService.getFilms()
+      .then(films => this.films = films.slice(1, 5));
+  }
+}
+
